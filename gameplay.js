e1071942c0ea49e0602329ba3b962e33fe619b39
YongshiSun <yongshisun3@gmail.com>
2026-08-02 17:36:33 -0700

    pages set up and working!

--- gameplay.js
+++ gameplay.js
@@ -1,3 +1,18 @@
-<!DOCTYPE HTML>
-<html>
-</html>
+createBook(
+    //left page content
+    `
+        <header> Gameplay Proejcts</header>
+        <ul class="toc_left toc">
+            <li id="toc_aboutme"><a href="aboutme.html">About me . . . . . . . . . . . . . . . . . . . . page 2</a></li>
+            <li id="toc_experiences"><a href="experiences.html">Experiences . . . . . . . . . . . . . . . . . . page 3</a></li>
+            <li id="toc_gameplay"><a href="gameplay.html">Gameplay Projects . . . . . . . . . . .  page 4</a></li>
+        </ul>
+    `,
+    //right page content
+    `
+        <ul class="toc_right toc">
+            <li id="toc_design"><a href="design.html">Design Projects . . . . . . . . . . . . .  page 5</a></li>
+            <li id="toc_contact"><a href="contact.html">Contact Info . . . . . . . . . . . . . .  . . page 6</a></li>
+        </ul>
+    `, 4, 3
+);
---
+++ header.js
@@ -0,0 +1,9 @@
+// just a page to put all my linked fonts so its easier to add to every other page
+document.head.insertAdjacentHTML("myheader", `
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css?family=Alex+Brush" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
+`);
